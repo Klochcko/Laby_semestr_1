a3356a23553be09eeb050d7533e50768bdcef07e
Dima <tukiu576@gmail.com>
2023-08-03 20:00:04 +0300

    изменил

--- Laba_7/Laba_7.3/Laba_7.3.cpp
+++ Laba_7/Laba_7.3/Laba_7.3.cpp
@@ -1,24 +1,16 @@
 #include <iostream>
 #include <ctime>
-
-void swap(char &a, char &b) {
-    char temp = a;
-    a = b;
-    b = temp;
-}
-
-void swap(float &a, float &b) {
-    float temp = a;
-    a = b;
-    b = temp;
-}
+#include <cstdlib>
 
 // Реалізація алгоритму сортування обміном (bubble sort) для масивів типу char
 void bubbleSort(char arr[], int size) {
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                swap(arr[j], arr[j + 1]);
+                // Обмін значень без додаткової функції swap
+                char temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
@@ -29,7 +21,10 @@ void bubbleSort(float arr[], int size) {
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                swap(arr[j], arr[j + 1]);
+                // Обмін значень без додаткової функції swap
+                float temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
@@ -103,75 +98,75 @@ int main() {
     double timeSmallChar_bubble = double(endTime - startTime) / CLOCKS_PER_SEC / iterations;
     std::cout << "Time taken for sorting array of size " << SIZE_SMALL << " (char) using Bubble Sort: " << timeSmallChar_bubble << " seconds" << std::endl;
 
-    // Сортування та вимірювання часу для масиву розміром 1000000 (char)
-    startTime = clock();
-    for (int i = 0; i < iterations; i++) {
-        bubbleSort(arr_large_char, SIZE_LARGE);
-    }
-    endTime = clock();
-    double timeLargeChar_bubble = double(endTime - startTime) / CLOCKS_PER_SEC / iterations;
-    std::cout << "Time taken for sorting array of size " << SIZE_LARGE << " (char) using Bubble Sort: " << timeLargeChar_bubble << " seconds" << std::endl;
+   // Сортування та вимірювання часу для масиву розміром 1000000 (char)
+   startTime = clock();
+   for (int i = 0; i < iterations; i++) {
+       bubbleSort(arr_large_char, SIZE_LARGE);
+   }
+   endTime = clock();
+   double timeLargeChar_bubble = double(endTime - startTime) / CLOCKS_PER_SEC / iterations;
+   std::cout << "Time taken for sorting array of size " << SIZE_LARGE << " (char) using Bubble Sort: " << timeLargeChar_bubble << " seconds" << std::endl;
 
-    std::cout << "Shell Sort for arrays of char..." << std::endl;
+   std::cout << "Shell Sort for arrays of char..." << std::endl;
 
-    // Сортування та вимірювання часу для масиву розміром 10 (char)
-    startTime = clock();
-    for (int i = 0; i < iterations; i++) {
-        shellSort(arr_small_char, SIZE_SMALL);
-    }
-    endTime = clock();
-    double timeSmallChar_shell = double(endTime - startTime) / CLOCKS_PER_SEC / iterations;
-    std::cout << "Time taken for sorting array of size " << SIZE_SMALL << " (char) using Shell Sort: " << timeSmallChar_shell << " seconds" << std::endl;
+   // Сортування та вимірювання часу для масиву розміром 10 (char)
+   startTime = clock();
+   for (int i = 0; i < iterations; i++) {
+       shellSort(arr_small_char, SIZE_SMALL);
+   }
+   endTime = clock();
+   double timeSmallChar_shell = double(endTime - startTime) / CLOCKS_PER_SEC / iterations;
+   std::cout << "Time taken for sorting array of size " << SIZE_SMALL << " (char) using Shell Sort: " << timeSmallChar_shell << " seconds" << std::endl;
 
-    // Сортування та вимірювання часу для масиву розміром 1000000 (char)
-    startTime = clock();
-    for (int i = 0; i < iterations; i++) {
-        shellSort(arr_large_char, SIZE_LARGE);
-    }
-    endTime = clock();
-    double timeLargeChar_shell = double(endTime - startTime) / CLOCKS_PER_SEC / iterations;
-    std::cout << "Time taken for sorting array of size " << SIZE_LARGE << " (char) using Shell Sort: " << timeLargeChar_shell << " seconds" << std::endl;
+   // Сортування та вимірювання часу для масиву розміром 1000000 (char)
+   startTime = clock();
+   for (int i = 0; i < iterations; i++) {
+       shellSort(arr_large_char, SIZE_LARGE);
+   }
+   endTime = clock();
+   double timeLargeChar_shell = double(endTime - startTime) / CLOCKS_PER_SEC / iterations;
+   std::cout << "Time taken for sorting array of size " << SIZE_LARGE << " (char) using Shell Sort: " << timeLargeChar_shell << " seconds" << std::endl;
 
-    std::cout << "Bubble Sort for arrays of float..." << std::endl;
+   std::cout << "Bubble Sort for arrays of float..." << std::endl;
 
-    // Сортування та вимірювання часу для масиву розміром 10 (float)
-    startTime = clock();
-    for (int i = 0; i < iterations; i++) {
-        bubbleSort(arr_small_float, SIZE_SMALL);
-    }
-    endTime = clock();
-    double timeSmallFloat_bubble = double(endTime - startTime) / CLOCKS_PER_SEC / iterations;
-    std::cout << "Time taken for sorting array of size " << SIZE_SMALL << " (float) using Bubble Sort: " << timeSmallFloat_bubble << " seconds" << std::endl;
+   // Сортування та вимірювання часу для масиву розміром 10 (float)
+   startTime = clock();
+   for (int i = 0; i < iterations; i++) {
+       bubbleSort(arr_small_float, SIZE_SMALL);
+   }
+   endTime = clock();
+   double timeSmallFloat_bubble = double(endTime - startTime) / CLOCKS_PER_SEC / iterations;
+   std::cout << "Time taken for sorting array of size " << SIZE_SMALL << " (float) using Bubble Sort: " << timeSmallFloat_bubble << " seconds" << std::endl;
 
-    // Сортування та вимірювання часу для масиву розміром 1000000 (float)
-    startTime = clock();
-    for (int i = 0; i < iterations; i++) {
-        bubbleSort(arr_large_float, SIZE_LARGE);
-    }
-    endTime = clock();
-    double timeLargeFloat_bubble = double(endTime - startTime) / CLOCKS_PER_SEC / iterations;
-    std::cout << "Time taken for sorting array of size " << SIZE_LARGE << " (float) using Bubble Sort: " << timeLargeFloat_bubble << " seconds" << std::endl;
+   // Сортування та вимірювання часу для масиву розміром 1000000 (float)
+   startTime = clock();
+   for (int i = 0; i < iterations; i++) {
+       bubbleSort(arr_large_float, SIZE_LARGE);
+   }
+   endTime = clock();
+   double timeLargeFloat_bubble = double(endTime - startTime) / CLOCKS_PER_SEC / iterations;
+   std::cout << "Time taken for sorting array of size " << SIZE_LARGE << " (float) using Bubble Sort: " << timeLargeFloat_bubble << " seconds" << std::endl;
 
-    std::cout << "Shell Sort for arrays of float..." << std::endl;
+   std::cout << "Shell Sort for arrays of float..." << std::endl;
+   
+   // Сортування та вимірювання часу для масиву розміром 10 (float)
+   startTime = clock();
+   for (int i = 0; i < iterations; i++) {
+       shellSort(arr_small_float, SIZE_SMALL);
+   }
+   endTime = clock();
+   double timeSmallFloat_shell = double(endTime - startTime) / CLOCKS_PER_SEC / iterations;
+   std::cout << "Time taken for sorting array of size " << SIZE_SMALL << " (float) using Shell Sort: " << timeSmallFloat_shell << " seconds" << std::endl;
 
-    // Сортування та вимірювання часу для масиву розміром 10 (float)
-    startTime = clock();
-    for (int i = 0; i < iterations; i++) {
-        shellSort(arr_small_float, SIZE_SMALL);
-    }
-    endTime = clock();
-    double timeSmallFloat_shell = double(endTime - startTime) / CLOCKS_PER_SEC / iterations;
-    std::cout << "Time taken for sorting array of size " << SIZE_SMALL << " (float) using Shell Sort: " << timeSmallFloat_shell << " seconds" << std::endl;
+   // Сортування та вимірювання часу для масиву розміром 1000000 (float)
+   startTime = clock();
+   for (int i = 0; i < iterations; i++) {
+       shellSort(arr_large_float, SIZE_LARGE);
+   }
+   endTime = clock();
+   double timeLargeFloat_shell = double(endTime - startTime) / CLOCKS_PER_SEC / iterations;
+   std::cout << "Time taken for sorting array of size " << SIZE_LARGE << " (float) using Shell Sort: " << timeLargeFloat_shell << " seconds" << std::endl;
 
-    // Сортування та вимірювання часу для масиву розміром 1000000 (float)
-    startTime = clock();
-    for (int i = 0; i < iterations; i++) {
-        shellSort(arr_large_float, SIZE_LARGE);
-    }
-    endTime = clock();
-    double timeLargeFloat_shell = double(endTime - startTime) / CLOCKS_PER_SEC / iterations;
-    std::cout << "Time taken for sorting array of size " << SIZE_LARGE << " (float) using Shell Sort: " << timeLargeFloat_shell << " seconds" << std::endl;
-
-    return 0;
+   return 0;
 }
 
