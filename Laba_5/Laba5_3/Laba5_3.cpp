83e912dc57fd3a060568da3e3e29a3e16699a631
Dima <tukiu576@gmail.com>
2023-08-06 12:52:50 +0300

    5.3 лаба

--- Laba_5/Laba5_3/Laba5_3.cpp
+++ Laba_5/Laba5_3/Laba5_3.cpp
@@ -1,30 +1,24 @@
-﻿#include <iostream>
+#include <iostream>
 
-// Шаблон з варіадичними шаблонами для визначення довжини масивів
 template <typename T>
-constexpr size_t length(const T& arr) {
+size_t length(const T& arr) {
     return sizeof(arr) / sizeof(arr[0]);
 }
 
-// Функція перетворення одномірного масиву у симетричний двомірний масив
-// Вхідні параметри: arr - одномірний масив, size - розмірність масиву
-// Повертає: двовимірний масив, симетричний відносно головної діагоналі
 int** convertToSymmetricMatrix(int* arr, size_t size) {
-    int** symmetricMatrix = new int* [size];
+    int** symmetricMatrix = new int*[size];
     for (size_t i = 0; i < size; i++) {
         symmetricMatrix[i] = new int[size];
         for (size_t j = 0; j < size; j++) {
             if (i >= j)
-                symmetricMatrix[i][j] = arr[i + j];
+                symmetricMatrix[i][j] = arr[i - j];
             else
-                symmetricMatrix[i][j] = arr[j + i];
+                symmetricMatrix[i][j] = arr[j - i];
         }
     }
     return symmetricMatrix;
 }
 
-// Функція відображення симетричного масиву
-// Вхідні параметри: symmetricMatrix - симетричний двомірний масив, size - розмірність масиву
 void displaySymmetricMatrix(int** symmetricMatrix, size_t size) {
     for (size_t i = 0; i < size; i++) {
         for (size_t j = 0; j < size; j++) {
@@ -34,17 +28,19 @@ void displaySymmetricMatrix(int** symmetricMatrix, size_t size) {
     }
 }
 
-// Функція відображення симетричного масиву відносно головної діагоналі
-// Вхідні параметри: symmetricMatrix - симетричний двомірний масив, size - розмірність масиву
 void reflectSymmetricMatrix(int** symmetricMatrix, size_t size) {
     for (size_t i = 0; i < size; i++) {
         for (size_t j = i + 1; j < size; j++) {
             symmetricMatrix[j][i] = symmetricMatrix[i][j];
         }
     }
+    for (size_t i = 0; i < size; i++) {
+        for (size_t j = 0; j < size / 2; j++) {
+            std::swap(symmetricMatrix[i][j], symmetricMatrix[i][size - j - 1]);
+        }
+    }
 }
 
-// Функція звільнення пам'яті, зайнятої двомірним масивом
 void deleteSymmetricMatrix(int** symmetricMatrix, size_t size) {
     for (size_t i = 0; i < size; i++) {
         delete[] symmetricMatrix[i];
@@ -53,23 +49,21 @@ void deleteSymmetricMatrix(int** symmetricMatrix, size_t size) {
 }
 
 int main() {
-    // Приклад вхідного одномірного масиву
-    int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    const size_t size = length(arr);
 
-    // Перетворення одномірного масиву у симетричний двомірний масив
-    int** symmetricMatrix = convertToSymmetricMatrix(arr, length(arr));
+    int** symmetricMatrix = convertToSymmetricMatrix(arr, size);
 
-    // Відображення симетричного двомірного масиву до відображення
-    std::cout << "До відображення:" << std::endl;
-    displaySymmetricMatrix(symmetricMatrix, length(arr));
+    std::cout << "Original Symmetric Matrix:" << std::endl;
+    displaySymmetricMatrix(symmetricMatrix, size);
 
-    // Відображення симетричного двомірного масиву після відображення
-    std::cout << "Після відображення:" << std::endl;
-    reflectSymmetricMatrix(symmetricMatrix, length(arr));
-    displaySymmetricMatrix(symmetricMatrix, length(arr));
+    reflectSymmetricMatrix(symmetricMatrix, size);
 
-    // Звільнення пам'яті, зайнятої двомірним масивом
-    deleteSymmetricMatrix(symmetricMatrix, length(arr));
+    std::cout << "Reflected Symmetric Matrix:" << std::endl;
+    displaySymmetricMatrix(symmetricMatrix, size);
+
+    deleteSymmetricMatrix(symmetricMatrix, size);
 
     return 0;
 }
+
