86f3c06ea090eefdc1a875f0652d864f0db09c17
Dima <tukiu576@gmail.com>
2023-09-19 18:26:36 +0300

    вроде бы оно

--- Laba_5/Laba_5.2/Laba_5.2.cpp
+++ Laba_5/Laba_5.2/Laba_5.2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <locale>
+#include <vector>
 
 int sum_of_digits(int num) {
     int sum_digits = 0;
@@ -10,54 +11,51 @@ int sum_of_digits(int num) {
     return sum_digits;
 }
 
-bool is_sum_of_digits_equal(int a, int b) {
-    return sum_of_digits(a) == b;
-}
-
-void filter_numbers(const int* input_numbers, int size, int target_sum, int* filtered_numbers, int& filtered_size) {
-    filtered_size = 0;
-    for (int i = 0; i < size; ++i) {
-        if (is_sum_of_digits_equal(input_numbers[i], target_sum)) {
-            filtered_numbers[filtered_size] = input_numbers[i];
-            ++filtered_size;
-        }
-    }
-}
-
 int main() {
+    std::locale::global(std::locale("ru_RU"));
 
     int size;
     std::cout << "Введите размер массива: ";
     std::cin >> size;
 
-    int* numbers = new int[size];
+    std::vector<int> numbers(size);
 
     std::cout << "Введите числа: ";
     for (int i = 0; i < size; ++i) {
         std::cin >> numbers[i];
     }
 
-    int* filtered_numbers = new int[size]; // Предполагаем, что размер фильтрованного массива не будет больше исходного
-    int filtered_size;
+    std::vector<int> filtered_numbers;
 
-    filter_numbers(numbers, size, sum_of_digits(numbers[0]), filtered_numbers, filtered_size);
+    for (int i = 0; i < size; ++i) {
+        int current_sum = sum_of_digits(numbers[i]);
+        bool found = false;
 
-    if (filtered_size > 0) {
-        std::cout << "Числа, сумма цифр которых равна сумме цифр первого числа: ";
-        for (int i = 0; i < filtered_size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            if (i != j && current_sum == numbers[j]) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            filtered_numbers.push_back(numbers[i]);
+        }
+    }
+
+    if (!filtered_numbers.empty()) {
+        std::cout << "Числа, сумма цифр которых равна другим числам в массиве: ";
+        for (int i = 0; i < filtered_numbers.size(); ++i) {
             std::cout << filtered_numbers[i];
-            if (i != filtered_size - 1) {
+            if (i != filtered_numbers.size() - 1) {
                 std::cout << ", ";
             }
         }
         std::cout << std::endl;
-    } else {
-        std::cout << "Нет чисел, сумма цифр которых равна сумме цифр первого числа." << std::endl;
     }
-
-    delete[] numbers;
-    delete[] filtered_numbers;
+    else {
+        std::cout << "Нет чисел, сумма цифр которых равна другим числам в массиве." << std::endl;
+    }
 
     return 0;
 }
-
