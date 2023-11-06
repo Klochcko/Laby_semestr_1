340c2407b760beae6b9a7955e2497196db232742
Dima <tukiu576@gmail.com>
2023-11-06 16:10:02 +0200

    а сейчас

--- Лаба_про_погоду_курс-2/Laba_1_new/Laba_1_new.cpp
+++ Лаба_про_погоду_курс-2/Laba_1_new/Laba_1_new.cpp
@@ -25,64 +25,127 @@ public:
     }
 };
 
+class Heap {
+public:
+    // Оголошення класу купи та його методів
+    // Додати елемент до купи
+    void insert(int value);
+
+    // Сортування купи
+    void sort();
+
+    // Вивести купу на екран
+    void print();
+};
+
+void Heap::insert(int value) {
+    // Додавання елемента до купи
+}
+
+void Heap::sort() {
+    // Пірамідальне сортування
+}
+
+void Heap::print() {
+    // Виведення купи на екран
+}
+
+class WeatherJournal {
+public:
+
+    std::vector<WeatherEntry> entries;
+
+    void addEntry(const WeatherEntry& entry) {
+        entries.push_back(entry);
+    }
+
+    // Генерація записів погоди для введеного користувачем року
+    void generateWeatherData(int inputYear) {
+        // Ініціалізуємо генератор випадкових чисел
+        std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+        for (int month = 1; month <= 12; month++) {
+            for (int day = 1; day <= 30; day++) {
+                // Створення запису погоди для кожного дня
+                std::string date = std::to_string(inputYear) + "-" + (month < 10 ? "0" : "") + std::to_string(month) + "-" + (day < 10 ? "0" : "") + std::to_string(day);
+                double temperature = 5.0 + (std::rand() % 10); // Генерація температури від 5.0 до 14.9
+                std::string windDirection = "Північ";
+                double windSpeed = 2.0 + (std::rand() % 10); // Генерація швидкості вітру від 2.0 до 11.9
+                double pressure = 1000.0 + (std::rand() % 50); // Генерація атмосферного тиску від 1000.0 до 1049.9
+                bool precipitation = (std::rand() % 2) == 1; // Випадково встановлено значення опадів
+
+                addEntry(WeatherEntry(date, temperature, windDirection, windSpeed, pressure, precipitation));
+            }
+        }
+    }
+
+    // Вивести місяці заданого користувачем року в порядку зменшення кількості днів з опадами
+    void printRainyMonths(int inputYear) {
+        std::map<std::string, int> rainyDays;
+
+        for (const WeatherEntry& entry : entries) {
+            int year = std::stoi(entry.date.substr(0, 4));
+            if (year == inputYear && entry.precipitation) {
+                std::string yearMonth = entry.date.substr(0, 7);
+                rainyDays[yearMonth]++;
+            }
+        }
+
+        if (!rainyDays.empty()) {
+            std::vector<std::pair<std::string, int>> monthsWithRain;
+            for (const auto& entry : rainyDays) {
+                monthsWithRain.push_back(entry);
+            }
+
+            std::sort(monthsWithRain.begin(), monthsWithRain.end(), [](const auto& a, const auto& b) {
+                return a.second > b.second;
+                });
+
+            std::cout << "Місяці року " << inputYear << " в порядку зменшення кількості днів з опадами:" << std::endl;
+            for (const auto& entry : monthsWithRain) {
+                std::cout << entry.first << " - " << entry.second << " днів з опадами" << std::endl;
+            }
+        }
+        else {
+            std::cout << "Немає даних про опади для року " << inputYear << std::endl;
+        }
+    }
+};
+
 int main() {
 
     std::locale::global(std::locale("uk_UA"));
 
-    const int maxEntries = 100;
-    std::vector<WeatherEntry> weatherJournal;
+    WeatherJournal weatherJournal;
 
-    // Генерація записів погоди для введеного користувачем року
+    // Введення року від користувача
     int inputYear;
     std::cout << "Введіть рік для генерації даних та підрахунку опадів: ";
     std::cin >> inputYear;
 
-    // Ініціалізуємо генератор випадкових чисел
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    // Генерація записів погоди для введеного року
+    weatherJournal.generateWeatherData(inputYear);
 
-    for (int month = 1; month <= 12; month++) {
-        for (int day = 1; day <= 30; day++) {
-            // Створення запису погоди для кожного дня
-            std::string date = std::to_string(inputYear) + "-" + (month < 10 ? "0" : "") + std::to_string(month) + "-" + (day < 10 ? "0" : "") + std::to_string(day);
-            double temperature = 5.0 + (std::rand() % 10); // Генерація температури від 5.0 до 14.9
-            std::string windDirection = "Північ";
-            double windSpeed = 2.0 + (std::rand() % 10); // Генерація швидкості вітру від 2.0 до 11.9
-            double pressure = 1000.0 + (std::rand() % 50); // Генерація атмосферного тиску від 1000.0 до 1049.9
-            bool precipitation = (std::rand() % 2) == 1; // Випадково встановлено значення опадів
+    // Виведення місяців з опадами
+    weatherJournal.printRainyMonths(inputYear);
 
-            weatherJournal.push_back(WeatherEntry(date, temperature, windDirection, windSpeed, pressure, precipitation));
-        }
-    }
+    // Пірамідальне сортування записів погоди
+    Heap heap; // Створюємо об'єкт класу купи
 
-    // Обчислення статистики опадів для введеного користувачем року
-    std::map<std::string, int> rainyDays;
+    // Додавання елементів в купу для сортування
+    heap.insert(5);
+    heap.insert(2);
+    heap.insert(7);
+    heap.insert(1);
 
-    for (const WeatherEntry& entry : weatherJournal) {
-        int year = std::stoi(entry.date.substr(0, 4));
-        if (year == inputYear && entry.precipitation) {
-            std::string yearMonth = entry.date.substr(0, 7);
-            rainyDays[yearMonth]++;
-        }
-    }
+    std::cout << "Елементи купи до сортування: ";
+    heap.print();
 
-    if (!rainyDays.empty()) {
-        std::vector<std::pair<std::string, int>> monthsWithRain;
-        for (const auto& entry : rainyDays) {
-            monthsWithRain.push_back(entry);
-        }
+    // Застосовуємо пірамідальне сортування
+    heap.sort();
 
-        std::sort(monthsWithRain.begin(), monthsWithRain.end(), [](const auto& a, const auto& b) {
-            return a.second > b.second;
-            });
-
-        std::cout << "Місяці року " << inputYear << " в порядку зменшення кількості днів з опадами:" << std::endl;
-        for (const auto& entry : monthsWithRain) {
-            std::cout << entry.first << " - " << entry.second << " днів з опадами" << std::endl;
-        }
-    }
-    else {
-        std::cout << "Немає даних про опади для року " << inputYear << std::endl;
-    }
+    std::cout << "Елементи купи після сортування: ";
+    heap.print();
 
     return 0;
 }
